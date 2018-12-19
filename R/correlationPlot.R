3e25bc9ccef028d009a14bf49625042e3d46966c
Taha Tehseen <33021614+ttehseen@users.noreply.github.com>
2018-12-19 18:42:46 +0500

    Edited correlationPlot.R and added screenshots for README

--- R/correlationPlot.R
+++ R/correlationPlot.R
@@ -1,9 +1,13 @@
-A <- matrix(sample(100, 1000, replace = T), ncol = 10)
-corr <- round(cor(A),3) 
+A <- matrix(sample(100, 1000, replace = T), ncol = 10) # A matrix with random numbers
+corr <- round(cor(A),3)  # Getting the correlation values
 corr.abs <- abs(corr)
-colnames(corr.abs) <- LETTERS[1:ncol(corr.abs)]
+colnames(corr.abs) <- LETTERS[1:ncol(corr.abs)] # Assigning column names
+rownames(corr.abs) <- LETTERS[1:ncol(corr.abs)] # and row names
+
+corr.abs # Let's have a look
+
+# The function
 correlationPlot <- function(corr) {
-  
   f <- colorRamp(c("white", "indianred3")) # Creating a color gradient
   colors <- rgb(f(corr.abs)/255)
   ax <- colnames(corr) # Axes labels for the plot
@@ -11,11 +15,14 @@ correlationPlot <- function(corr) {
   par(xpd = TRUE) # To position the legend out of the plot
   symbols(x = rep_len(1:mat.length, mat.length**2), 
           y = rep(1:mat.length, each = mat.length), 
-          circles = rep(1, mat.length**2), inches = 0.1, bg = colors, 
+          circles = rep(1, mat.length**2), inches = 0.175, bg = colors, 
           xlab = "", ylab = "", main = "Correlation between variables",
           axes = F)
   axis(1, 1:mat.length, ax, las = 2, line = -1.5)
   axis(2, 1:mat.length, ax, las = 2, line = -1.5)
+  text(x = rep_len(1:mat.length, mat.length**2), 
+       y = rep(1:mat.length, each = mat.length), 
+       labels = corr, cex = 0.5)
   legend(-2,12.5,c("No Correlation", "Perfect Correlation"), 
          pch = 21, pt.bg = c("white", "indianred3"), pt.cex = 2,
          cex = 0.8)
